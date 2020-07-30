d01f26f0954255ddd518c5e0bb08959174e2606e
ChipCE <login721@gmail.com>
2020-07-31 08:41:57 +0900

    change MANUAL_*_HOME_POS to +_MIN_POS

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1136,8 +1136,8 @@
 #define Y_BED_SIZE 215
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define X_MIN_POS -27
+#define Y_MIN_POS -1
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
@@ -1382,8 +1382,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS -27
-#define MANUAL_Y_HOME_POS -1
+//#define MANUAL_X_HOME_POS -27
+//#define MANUAL_Y_HOME_POS -1
 //#define MANUAL_Z_HOME_POS 0
 
 // Use "Z Safe Homing" to avoid homing with a Z probe outside the bed area.
